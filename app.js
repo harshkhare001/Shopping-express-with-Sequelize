7f01aba623fbc39b1bbeb9c4382bd9b4da07f681
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-10-19 14:19:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequilze =require('./util/database');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,14 +20,33 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next)=>{
+    User.findByPk(1).then((user)=>{
+        req.user = user;
+        next();
+    })
+    .catch((err)=>console.log(err));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User,{constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequilze.sync()
 .then((result)=>{
-    console.log(result);
+    return User.findByPk(1);
+})
+.then((user)=>{
+    if(!user){
+        return User.create({name:'Harsh' , email:"harshkhare2020@gmail.com"});
+    }
+    return user;
+})
+.then((result)=>{
     app.listen(3000);
 })
 .catch((err)=>{
